5e4487ea2c5b5b116fec55fb2faef35ef867bc9b
Julien Sansot <juliensansot@gmail.com>
2018-10-04 00:48:07 +0100

    adding optional inputs and more validation

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -3,11 +3,13 @@ class Lead
 
   attr_reader :first_name, :last_name, :business_name, :telephone_number, :email, :contact_time, :reference, :notes
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :business_name, presence: true
-  validates :telephone_number, presence: true
-  validates :email, presence: true
+  validates :first_name, presence: true, length: { maximum: 49 }
+  validates :last_name, presence: true, length: { maximum: 49 }
+  validates :business_name, presence: true, length: { maximum: 100 }
+  validates :telephone_number, presence: true, length: { maximum: 13 }, phone: true
+  validates :email, presence: true, length: { maximum: 80 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :reference, length: { maximum: 50 }
+  validates :notes, length: { maximum: 255 }
 
   def initialize(first_name: nil, last_name: nil, business_name: nil, telephone_number: nil, email: nil, contact_time: nil, reference: nil, notes: nil)
     @first_name = first_name
